83a5f81751ade436b2486301d94455861a0539bf
omrkhoder <49230362+omrkhoder@users.noreply.github.com>
2019-10-07 05:29:37 +0200

    Update itemsofmenu.py

--- itemsofmenu.py
+++ itemsofmenu.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Category, Base, Item, User
 
-engine = create_engine('sqlite:///itemcatalog.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
+#engine = create_engine('sqlite:///itemcatalog.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
